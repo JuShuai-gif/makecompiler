5ddf11ba495e83d5735fa9bec047d6e24cf9a1b4
ghr <208967048@qq.com>
2025-09-03 03:08:22 +0000

    add

--- utils/test/graph_test.c
+++ utils/test/graph_test.c
@@ -1,20 +1,62 @@
-#include"../utils_stack.h"
+#include "../utils_graph.h"
 
-int main()
-{
-	stack_t* s = stack_alloc();
+int main(){
+    graph_t* g = graph_alloc();
 
-	int i;
-	for (i = 0; i < 64; i++) {
-		printf("i: %d, s->size: %d, s->capacity: %d\n", i, s->size, s->capacity);
-		stack_push(s, (void*)i);
-	}
+    graph_node_t* n0 = graph_node_alloc();
+    graph_node_t* n1 = graph_node_alloc();
+    graph_node_t* n2 = graph_node_alloc();
+    graph_node_t* n3 = graph_node_alloc();
+    graph_node_t* n4 = graph_node_alloc();
+    graph_node_t* n5 = graph_node_alloc();
 
-	for (i = 0; i < 64; i++) {
-		int j = (int)stack_pop(s);
-		printf("i: %d, j: %d, s->size: %d, s->capacity: %d\n", i, j, s->size, s->capacity);
-	}
+    graph_make_edge(n1,n2);
+    graph_make_edge(n2,n3);
+    graph_make_edge(n3,n0);
 
-	int j = (int)stack_pop(s);
-	printf("i: %d, j: %d, s->size: %d, s->capacity: %d\n", i, j, s->size, s->capacity);
+    graph_add_node(g,n0);
+    graph_add_node(g,n3);
+    graph_add_node(g,n2);
+    graph_add_node(g,n1);
+
+    graph_make_edge(n0,n1);
+    graph_make_edge(n1,n0);
+
+    int nb_colors = 2;
+    vector_t* colors = vector_alloc();
+    int i;
+    for (i = 0; i < nb_colors; i++)
+        vector_add(colors,(void*)(intptr_t)(i+1));
+
+    graph_kcolor(g,colors);
+
+    graph_node_print(n0);
+    graph_node_print(n1);
+    graph_node_print(n2);
+    graph_node_print(n3);
+
+    return 0;
+    
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
